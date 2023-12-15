2345e56945b2365d69cd5ce5258ddfeb828e4a44
Vadim Sysoev <sysoev.dev@gmail.com>
2023-12-15 17:51:49 +0300

    fix: format code

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/header'
+import './App.css';
+import Header from './components/header';
 
 function App() {
-
-
   return (
     <>
-      <div className="container">
+      <div className='container'>
         <h1 className='visually-hidden'>Books - Книжный интернет-магазин</h1>
         <Header />
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,4 @@
-import './index.css'
+import './index.css';
 import bookIcon from '../../assets/book.svg';
 import headphonesIcon from '../../assets/headphones.svg';
 import favoriteIcon from '../../assets/favorite.svg';
@@ -9,42 +9,44 @@ import dotsIcon from '../../assets/dots.svg';
 
 export default function Header() {
   return (
-    <header className="header">
-      <nav className="header__nav nav">
-        <a className="nav__logo-link" href="/index.html">Books</a>
-        <ul className="nav__list">
-          <li className="nav__item">
-            <a href="#" className="nav__link">
-              <img src={bookIcon} alt="Иконка книги" />
+    <header className='header'>
+      <nav className='header__nav nav'>
+        <a className='nav__logo-link' href='/index.html'>
+          Books
+        </a>
+        <ul className='nav__list'>
+          <li className='nav__item'>
+            <a href='#' className='nav__link'>
+              <img src={bookIcon} alt='Иконка книги' />
               Книги
             </a>
           </li>
-          <li className="nav__item">
-            <a href="#" className="nav__link">
-              <img src={headphonesIcon} alt="Иконка аудиокниги" />
+          <li className='nav__item'>
+            <a href='#' className='nav__link'>
+              <img src={headphonesIcon} alt='Иконка аудиокниги' />
               Аудиокниги
             </a>
           </li>
         </ul>
-        <div className="nav__user-box">
+        <div className='nav__user-box'>
           <button className='nav__user-btn nav__favorite-btn'>
-            <img src={favoriteIconOutline} alt="Избранное" />
+            <img src={favoriteIconOutline} alt='Избранное' />
             Добавить в Избранное
           </button>
           <button className='nav__user-btn nav__favorite-btn'>
-            <img src={basketIcon} alt="Корзина покупок" />
+            <img src={basketIcon} alt='Корзина покупок' />
             Корзина
           </button>
           <button className='nav__user-btn nav__favorite-btn'>
-            <img src={userIcon} alt="Иконка профиля" />
+            <img src={userIcon} alt='Иконка профиля' />
             Профиль
           </button>
           <button className='nav__user-btn nav__favorite-btn'>
-            <img src={dotsIcon} alt="Настройки" />
+            <img src={dotsIcon} alt='Настройки' />
             Настройки
           </button>
         </div>
       </nav>
     </header>
-  )
+  );
 }
